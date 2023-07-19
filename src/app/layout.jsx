c9b5902cf6e4272edb4c3d6af6d3bdfea9d5753c
LaurentiuGhiga <ghiga_laurentiu@yahoo.com>
2023-07-20 00:27:43 +0200

    install tailwind, header 1/2

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,12 +1,23 @@
+import Header from "@/components/Header";
 import "./globals.css";
 import Head from "./head";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <Head />
-      
-      <body >{children}</body>
+      <Head />
+
+      <body>
+        {/* Header */}
+        <Header />
+
+        {/* Navbar */}
+
+        {/* SearchBox */}
+
+        {children}
+        
+      </body>
     </html>
   );
 }
