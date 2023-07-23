c8d2a538dde95a1045a99858f4c21ea311b35916
LaurentiuGhiga <ghiga_laurentiu@yahoo.com>
2023-07-23 12:51:24 +0200

    add products

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,12 @@
+import ProductItem from "@/components/ProductItem";
+import data from "../../utils/data";
 
 export default function Home() {
   return (
-    <h1 className="text-red-300">HOME</h1>
+    <div className="grid gric-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 p-10 justify-center">
+      {data.products.map((product) => (
+        <ProductItem product={product} key={product.slug}></ProductItem>
+      ))}
+    </div>
   )
 }
--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -7,7 +7,7 @@ export default function NavbarItem({ title, param }) {
     const collection = searchParam.get("collection");
   return (
     <div>
-      <Link className={`m-4 hover:text-amber-600 font-semibold p-2 ${collection && collection === param && "underline underline-offset-8 decoration-4 decoration-amber-500 decoration-rounded-lg"}`} href={`/?collection=${param}`}>{title}</Link>
+      <Link className={`m-4 hover:text-amber-600 font-semibold p-2 ${collection && collection === param && "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"}`} href={`/?collection=${param}`}>{title}</Link>
     </div>
   );
 }
