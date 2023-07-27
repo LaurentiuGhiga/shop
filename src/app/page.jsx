6db836603e7d3c2608a1c203a25f208ea0caa19a
LaurentiuGhiga <ghiga_laurentiu@yahoo.com>
2023-07-27 20:46:23 +0200

    handle the errors

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,12 +2,16 @@
 import ProductItem from "@/components/ProductItem";
 import data from "../../utils/data";
 import Results from "@/components/Results";
-const results = data.products;
 
 export default async function Home() {
+  const results = data.products;
+ 
+  if (!results){
+    throw new Error("Failed to fetch data");
+  }
   
   return (
-    <div className="grid gric-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 p-10 justify-center">
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 p-10 justify-center">
       <Results results={results}/>
     </div>
   )
